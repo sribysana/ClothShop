204dc9754369390da5ff47f863c3eaf9d21cbdcd
Srikanth Bysana <sribysana@gmail.com>
2020-06-03 21:57:09 +0530

    Updates

---
+++ precache-manifest.14c4d59c194d5c60605429df994dad6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6930c7c9d73b5034b7fe6d9e5b4859",
+    "url": "/ClothShop/index.html"
+  },
+  {
+    "revision": "347b7534ce84d883cb17",
+    "url": "/ClothShop/static/css/main.186dcff6.chunk.css"
+  },
+  {
+    "revision": "b8c38ffe0189ae68f8c9",
+    "url": "/ClothShop/static/js/2.9122c6ee.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/ClothShop/static/js/2.9122c6ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "347b7534ce84d883cb17",
+    "url": "/ClothShop/static/js/main.2926c634.chunk.js"
+  },
+  {
+    "revision": "3e858bf49ebae9a89a7e",
+    "url": "/ClothShop/static/js/runtime-main.6526864a.js"
+  },
+  {
+    "revision": "f3042a4925902b65de5e5d346d737f31",
+    "url": "/ClothShop/static/media/crown.f3042a49.svg"
+  }
+]);
